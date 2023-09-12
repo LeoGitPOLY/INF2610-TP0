cb3aecbe9e08375f28d34c99874c6db24d94f407
Aurélie  Nichols <aurelie.nichols@polymtl.ca>
2023-09-12 18:31:26 -0400

    Ajout de l'erreur part1

--- part1.c
+++ part1.c
@@ -6,22 +6,24 @@
  * Leonard Pouliot - 2150965
  * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
  */
-
-// TODO
 // Si besoin, ajouter ici les directives d'inclusion et les déclarations globales
-// -------------------------------------------------
-
-   
 // -------------------------------------------------
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+// -------------------------------------------------
 
 int main () {
     char textInFile;
     char docPath[] = "./output2.txt";
-    int fd = open(docPath, O_WRONLY | O_TRUNC);
     int userInput = 0;
+
+    int fd = open(docPath, O_WRONLY | O_TRUNC);
+    if(fd == -1){
+        printf("Appel système open a échoué\n");
+        return 0;
+    }
+    
     printf("Saisissez votre texte suivi de CTRL-D :\n");
     
     while (read(userInput, &textInFile, 1)){

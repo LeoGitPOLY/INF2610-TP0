f8ffd34770d9358f9354de875fc432d0dc9e8322
Aurélie  Nichols <aurelie.nichols@polymtl.ca>
2023-09-12 18:21:19 -0400

    Petites modifications

--- part1.c
+++ part1.c
@@ -3,6 +3,7 @@
  * Automne  2022
  * Initlab - part1.c
  * Aurelie Nichols - 2142404
+ * Leonard Pouliot - 2150965
  * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
  */
 
@@ -17,14 +18,13 @@
 #include <stdio.h>
 
 int main () {
-    // TODO
     char textInFile;
     char docPath[] = "./output2.txt";
     int fd = open(docPath, O_WRONLY | O_TRUNC);
-    int userStream = 0;
+    int userInput = 0;
     printf("Saisissez votre texte suivi de CTRL-D :\n");
     
-    while (read(userStream, &textInFile, 1)){
+    while (read(userInput, &textInFile, 1)){
         write(fd, &textInFile, 1);
     }
     close(fd);

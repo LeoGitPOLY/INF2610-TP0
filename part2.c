0b2198436e43c5fffe291dfb2e3b0b145b635814
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-09-12 17:29:40 -0400

    last push periode

--- part2.c
+++ part2.c
@@ -4,7 +4,7 @@
  * Initlab - part2.c
  *
  * ajoutez vos noms, prénoms et matricules
-*/
+ */
 #include <stdio.h>
 #include <stdlib.h>
 // Si besoin, ajouter ici les directives d'inclusion et les déclarations globales
@@ -12,33 +12,43 @@
 // TODO
 
 // -------------------------------------------------
-void part21 ()
+void part21()
 {
- // TODO
-}
+    char strToPrint[] = "77dbcb01f571f1c32e196c3a7d27f62e";
+    int lenStr = 32;
 
-void part22 ()
-{
- // TODO
-}
-
-
-
-int main (int argc, char* argv[])
-{
+    for (size_t i = 0; i < lenStr; i++)
+    {
+        printf("%c", strToPrint[i]);
+    }
     
-    if (argc!=2)
-    {   printf("Le programme a un seul paramètre : 1 ou 2\n");
+    printf("\n");
+}
+
+void part22()
+{
+    // TODO
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("Le programme a un seul paramètre : 1 ou 2\n");
         return 1;
     }
-    switch (atoi(argv[1])) {
-         case 1:        part21();
-                        break;
-         case 2:        part22();
-                        break;
-        default:        printf(" Le paramètre du programme est invalide\n");
-                        return 1;
+    switch (atoi(argv[1]))
+    {
+    case 1:
+        part21();
+        break;
+    case 2:
+        part22();
+        break;
+    default:
+        printf(" Le paramètre du programme est invalide\n");
+        return 1;
     }
     return 0;
 }
-

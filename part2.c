f7bc5c8d3ff4ea187c08630679754eb691eaa474
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-09-12 20:24:52 -0400

    Question 2.2 et 2.3

--- part2.c
+++ part2.c
@@ -2,32 +2,36 @@
  * Ecole polytechnique de Montreal - GIGL
  * Automne  2022
  * Initlab - part2.c
- *
- * ajoutez vos noms, prénoms et matricules
+ * Aurelie Nichols - 2142404
+ * Leonard Pouliot - 2150965
  */
-#include <stdio.h>
-#include <stdlib.h>
 // Si besoin, ajouter ici les directives d'inclusion et les déclarations globales
 // -------------------------------------------------
-// TODO
-
+#include <stdio.h>
+#include <stdlib.h>
 // -------------------------------------------------
+
 void part21()
 {
-    char strToPrint[] = "77dbcb01f571f1c32e196c3a7d27f62e";
-    int lenStr = 32;
+    const int STD_OUT = 1;
+    char strToPrint[] = "77dbcb01f571f1c32e196c3a7d27f62e (printed using fprintf)";
+    char strToWrite[] = "77dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n";
+    int lenStroPrint = 57;
+    int lenStroWrite = 56;
 
-    for (size_t i = 0; i < lenStr; i++)
+    for (size_t i = 0; i < lenStroPrint; i++)
     {
         printf("%c", strToPrint[i]);
     }
-    
+
+    write(STD_OUT, strToWrite, lenStroWrite);
     printf("\n");
 }
 
 void part22()
 {
-    // TODO
+    setvbuf(stdout, NULL, _IONBF, 0);
+    part21();
 }
 
 int main(int argc, char *argv[])
